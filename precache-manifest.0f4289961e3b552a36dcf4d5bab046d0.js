34bf17a9774a1c18acef3d81fdcb0323970d7bc9
fortnite-companion <50246185+fortnite-companion@users.noreply.github.com>
2019-05-10 18:12:34 +0200

    Add files via upload

---
+++ precache-manifest.0f4289961e3b552a36dcf4d5bab046d0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d47605fb4412507e968de4feaaff1e4b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5588c899a005dac03604",
+    "url": "/static/css/main.a72b48cc.chunk.css"
+  },
+  {
+    "revision": "aa0e0cf8ecd223d32ebc",
+    "url": "/static/js/2.523e5aa0.chunk.js"
+  },
+  {
+    "revision": "5588c899a005dac03604",
+    "url": "/static/js/main.f779c82c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
