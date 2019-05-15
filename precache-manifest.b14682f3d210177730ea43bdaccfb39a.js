3a7af7bc8a069af62b5b3c5f4b0fa8c0617e4e32
fortnite-companion <50246185+fortnite-companion@users.noreply.github.com>
2019-05-15 18:26:43 +0200

    Add files via upload

---
+++ precache-manifest.b14682f3d210177730ea43bdaccfb39a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a23a2c37dc665f223714dcbe9904d986",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6dab1ead07ada249987b",
+    "url": "/static/css/main.c59fbdc4.chunk.css"
+  },
+  {
+    "revision": "96c5376c9b6223b372bc",
+    "url": "/static/js/2.aab451e3.chunk.js"
+  },
+  {
+    "revision": "6dab1ead07ada249987b",
+    "url": "/static/js/main.c2a066bd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "de4170dada1c5c991181c8d463324b0e",
+    "url": "/static/media/fnitebg.de4170da.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b9156d3fcb37095d2a28b565c2993052.js"
+  "/precache-manifest.b14682f3d210177730ea43bdaccfb39a.js"
 );
 
 self.addEventListener('message', (event) => {
